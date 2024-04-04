9961fce280c3f85c0a7a99593d1c22611116eec1
Rodrijojo <marcosrodriguez3000@gmail.com>
2024-04-04 12:59:43 -0300

    agregue gitignore

--- react_game/src/App.jsx
+++ react_game/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import confetti from 'canvas-confetti'
 
-import { Square } from "./components/Square.jsx"
+import { Square } from './components/Square.jsx'
 import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame} from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
